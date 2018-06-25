8fcae913c4753e721c6518da6db908d6d4167fe7
crunchtimer.yoshikawa <yoshikawada@crunchtimer.jp>
2018-06-25 16:29:38 +0900

    postgres->mysql

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v `pwd`:/myproject dyoshikawa/rails-api:latest bundle install --path=vendor/bundle
+docker run --rm -v `pwd`:/myproject dyoshikawa/rails-api:latest bundle install --path=vendor/bundle --jobs=4
